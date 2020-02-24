f99befd81c82ccbac825032b7cf5cdb7a55bc48b
fangfangxu <472848022@qq.com>
2020-02-24 21:50:58 +0800

    commit
    commit

---
+++ src/main/java/com/xufangfang/o2o/dao/ShopDao.java
@@ -0,0 +1,13 @@
+package com.xufangfang.o2o.dao;
+
+import com.xufangfang.o2o.entity.Shop;
+
+public interface ShopDao {
+    /**
+     * 新增店铺
+     *
+     * @param shop
+     * @return
+     */
+    int insertShop(Shop shop);
+}
---
+++ src/test/java/com/xufangfang/o2o/dao/ShopDaoTest.java
@@ -0,0 +1,41 @@
+package com.xufangfang.o2o.dao;
+
+import com.xufangfang.o2o.BaseTest;
+import com.xufangfang.o2o.entity.Area;
+import com.xufangfang.o2o.entity.PersonInfo;
+import com.xufangfang.o2o.entity.Shop;
+import com.xufangfang.o2o.entity.ShopCategory;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+public class ShopDaoTest extends BaseTest {
+    @Autowired
+    private ShopDao shopDao;
+
+    @Test
+    public void testInsertShop() {
+        Shop shop = new Shop();
+        PersonInfo owner = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        owner.setUserId(1L);
+        area.setAreaId(2);
+        shopCategory.setShopCategoryId(10L);
+        shop.setOwner(owner);
+        shop.setShopCategory(shopCategory);
+        shop.setArea(area);
+        shop.setShopName("测试的店铺");
+        shop.setShopDesc("testxu");
+        shop.setShopAddr("test");
+        shop.setPhone("test");
+        shop.setShopImg("test");
+        shop.setCreateTime(new Date());
+        shop.setEnableStatus(1);
+        shop.setAdvice("审核中");
+        int effectedNum=shopDao.insertShop(shop);
+    }
+
+
+}
