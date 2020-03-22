1ead6768a1401298131b0b2031a30b707bdcd8d2
fangfangxu <472848022@qq.com>
2020-03-22 23:56:00 +0800

    commit
    commit

--- src/main/webapp/resources/js/shop/shopoperation.js
+++ src/main/webapp/resources/js/shop/shopoperation.js
@@ -33,7 +33,7 @@ $(function () {
                 $('#shop-category').html(shopCategory);
                 $('#shop-category').attr('disabled', 'disabled');
                 $('#area').html(tempAreaHtml);
-                $('#area').attr('data-id', shop.areaId);
+                $("#area option[data-id='"+shop.area.areaId+"']").attr("selected","selected");
             }
         });
     }
@@ -54,54 +54,59 @@ $(function () {
                 $('#area').html(tempAreaHtml);
             }
         });
-        $('#submit').click(function () {
-            var shop = {};
-            shop.shopName = $('#shop-name').val();
-            shop.shopAddr = $('#shop-address').val();
-            shop.phone = $('#shop-phone').val();
-            shop.shopDesc = $('#shop-desc').val();
-            shop.shopCategory = {
-                shopCategoryId: $('#shop-category').find('option').not(function () {
-                    return !this.selected;
-                }).data('id')
-            };
-            shop.area = {
-                areaId: $('#area').find('option').not(function () {
-                    return !this.selected;
-                }).data('id')
-            }
-            var shopImg = $('#shop-img')[0].files[0];
-            var formData = new FormData();
-            formData.append('shopImg', shopImg);
-            formData.append('shopStr', JSON.stringify(shop));
-
-            var verifyCodeActual = $('#j_captcha').val();
-            // alert(verifyCodeActual);
-            if (!verifyCodeActual) {
-                $.toast('请输入验证码！');
-                return;
-            }
-            formData.append('verifyCodeActual', verifyCodeActual);
-            // $('#captcha_img').click();
-            $.ajax(
-                {
-                    url: (isEdit ? editShopUrl : registerShopUrl),
-                    type: 'POST',
-                    data: formData,
-                    contentType: false,
-                    processData: false,
-                    cache: false,
-                    success: function (data) {
-                        if (data.success) {
-                            $.toast('提交成功!');
-                        } else {
-                            $.toast('提交失败!' + data.errMsg);
-                        }
-                        $('#captcha_img').click();
-                    }
-                }
-            );
-        });
 
     }
+
+    $('#submit').click(function () {
+        var shop = {};
+        if(isEdit){
+            shop.shopId=shopId;
+        }
+        shop.shopName = $('#shop-name').val();
+        shop.shopAddr = $('#shop-address').val();
+        shop.phone = $('#shop-phone').val();
+        shop.shopDesc = $('#shop-desc').val();
+        shop.shopCategory = {
+            shopCategoryId: $('#shop-category').find('option').not(function () {
+                return !this.selected;
+            }).data('id')
+        };
+        shop.area = {
+            areaId: $('#area').find('option').not(function () {
+                return !this.selected;
+            }).data('id')
+        }
+        var shopImg = $('#shop-img')[0].files[0];
+        var formData = new FormData();
+        formData.append('shopImg', shopImg);
+        formData.append('shopStr', JSON.stringify(shop));
+
+        var verifyCodeActual = $('#j_captcha').val();
+        // alert(verifyCodeActual);
+        if (!verifyCodeActual) {
+            $.toast('请输入验证码！');
+            return;
+        }
+        formData.append('verifyCodeActual', verifyCodeActual);
+        // $('#captcha_img').click();
+        $.ajax(
+            {
+                url: (isEdit ? editShopUrl : registerShopUrl),
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                processData: false,
+                cache: false,
+                success: function (data) {
+                    if (data.success) {
+                        $.toast('提交成功!');
+                    } else {
+                        $.toast('提交失败!' + data.errMsg);
+                    }
+                    $('#captcha_img').click();
+                }
+            }
+        );
+    });
+
 })
